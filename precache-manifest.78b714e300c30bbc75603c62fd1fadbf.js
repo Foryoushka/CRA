431b1f66c81d61180b482785ca5c118f6569a06f
Foryoushka <egboytsova@gmail.com>
2020-02-16 20:57:36 -0500

    Updates

---
+++ precache-manifest.78b714e300c30bbc75603c62fd1fadbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ccd4809aa2b836fe2e568ebdb1e242c",
+    "url": "/cra/index.html"
+  },
+  {
+    "revision": "315a4421bb742972dba0",
+    "url": "/cra/static/css/main.013436ae.chunk.css"
+  },
+  {
+    "revision": "4b705c80edf65ac33df3",
+    "url": "/cra/static/js/2.5911d2c1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra/static/js/2.5911d2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315a4421bb742972dba0",
+    "url": "/cra/static/js/main.ed914dbb.chunk.js"
+  },
+  {
+    "revision": "2c62cba4c4c2d636f57d",
+    "url": "/cra/static/js/runtime-main.fe26b8f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra/static/media/logo.5d5d9eef.svg"
+  }
+]);
